e8541c544214470ae9f3d4886dcf5ac7ac324600
Darima Motta <darima_mtp@mail.ru>
2021-08-14 23:53:26 +0300

    ES6 Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+/*jshint esversion: 6 */
+class Rectangle {
+    constructor(height, width) {
+        this.height=height;
+        this.width=width;
+        
+    }
+    calcArea(){
+        return this.height*this.width;
+    }
+}
+class ColoredRectangleWithText extends Rectangle{
+    constructor(height,width, text, bgColor){
+        super(height,width);
+        this.text=text;
+        this.bgColor=bgColor;
+    }
+    showMyProps(){
+        console.log(`text:${this.text},color:${this.bgColor}`);
+    }
+}
+const div=new ColoredRectangleWithText(25,10,'Hello World','red');
+div.showMyProps();
+console.log(div.calcArea());
+//const square=new Rectangle(10,10);
+//const long=new Rectangle(20,100);
+//console.log(long.calcArea());
+//console.log(square.calcArea());
